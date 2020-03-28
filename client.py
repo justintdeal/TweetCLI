62801483063ab0561571c4e8b5c3d27771767f27
Evie Taylor <evietaylor@Evies-MacBook-Pro.local>
2020-03-27 21:32:55 -0400

    all of my effort thus far

--- client.py
+++ client.py
@@ -1,18 +1,36 @@
 from socket import * 
 import sys
 import re
+import _thread as thread
 
 def main():
     host, port, user = validateArgs(sys.argv)
     print(sys.argv)
     csock = connect(host, port, user)
     while True:
-        input("Options: tweet, subscribe, unsubscribe, timeline, users, tweets, exit")
+        thread.start_new_thread(listening, (csock,))
+        raw = input("Options: tweet, subscribe, unsubscribe, timeline, users, tweets, exit\n").split()
+        print(raw)
+        if (len(raw) == 2 and raw[0] == "subscribe" and validateTag(raw[1])):
+            subscribe(user, raw[1], csock)
+        if (len(raw) == 3 and raw[0] == "tweet"): 
+            valid = True
+            if (len(raw[1]) <= 2): 
+                valid = False
+                print("message format illegal.")
+            if (len(raw[1]) >= 150):
+                valid = False 
+                print("message length illegal, connection refused.")
+            if (valid):
+                tweet((raw[1] +" "+ raw[2]), csock)
 
-def tweet():
+def tweet(message, conn):
+    code = "tweet " + str(message)
+    conn.sendall(code.encode())
 
-
-# def subscribe(): 
+def subscribe(user, tag, conn): 
+    code = "sub " + str(user) + " " + str(tag)
+    conn.sendall(code.encode())
 
 # def unsubscribe():
 
@@ -39,6 +57,14 @@ def connect(host, port, user):
         print('username illegal, connection refused')
         sys.exit()
 
+def listening(conn):
+    while True:
+        data = conn.recv(1024).decode().split()
+        if data[0] == "rep":
+            print(str(data[1]) + " " + str(data[2]) + " " +str(data[3]))
+        if data [0] == "good":
+            print("operation success")
+
 def validateArgs(args):
     regexIP = '''^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.( 
             25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.( 
@@ -49,16 +75,32 @@ def validateArgs(args):
     if len(args) != 4: 
         print('error: args should contain <ServerIP> <Server Port> <Username>')
         sys.exit()
-    host, port, user = args[1], args[2], args[3]
+    host, port, user = args[1], int(args[2]), args[3]
     if port < 1024 or port > 65535: 
         print('error: server port invalid, connection refused')
         sys.exit()
-    if not re.serach(regexIP, host):
+    if not re.search(regexIP, host):
        print('error: server ip invalid, connection refused') 
        sys.exit()
     if not re.search(regexUser, user):
         print('error: username has wrong format, connection refused')
         
     return host, port, user
+
+def validateTag(tag):
+    valid = True
+    for index in range(len(tag)-1):
+        if tag[index] == tag[index+1]:
+            if tag[index] == "#":
+                valid = False
+                print('hashtag illegal format, connection refused.')
+    return valid
+
+
+            
+
+
+    
+
     
 main()
--- server.py
+++ server.py
@@ -3,11 +3,15 @@ import sys
 import _thread as thread
 
 activeUsers = []
+userToPort = dict()
+hashToUser = dict()
+userToHash = dict()
 
 def main(): 
     host = '127.0.0.1'
     port = validPort(sys.argv)
     sock = getAndBindSocket(host, port)
+
     while True: 
         conn, addr = sock.accept()
         user = conn.recv(1024).decode()
@@ -17,13 +21,44 @@ def main():
         else:
             conn.sendall("True".encode())
             activeUsers.append(user)
+            if (user not in userToHash.keys()):
+                userToHash[user] = []
+            userToPort[user] = conn
             thread.start_new_thread(newClient, (conn,))
+        
 
 def newClient(conn):
+
     while True:
         print('newClient')
-        data = conn.recv(1024).decode()
-        conn.sendall(data.enconde())
+        data = conn.recv(1024).decode().split()
+        if (data[0] == "sub"): #putting hashtags away
+            user = data[1] #saving data
+            tag = data[2]
+            print(tag) #get rid of later
+            if (tag not in hashToUser.keys()): # add hashtag to keys if not already saved 
+                hashToUser[tag] = []
+            
+            if (len(userToHash[user]) >= 3): #lenght check for subscriptions
+                print("sub <hashtag> failed, already exists or exceeds 3 limitation")
+                # sys.exit()
+            if (user not in hashToUser[tag]): #add user to tag key
+                hashToUser[tag].append(user)
+            if (tag not in userToHash[user]): #add tag to user key
+                userToHash[user].append(tag)
+            conn.sendall(("good").encode())
+
+    
+        if (data[0] == "tweet"): #if tweet is called
+            message = data[1] #saving data
+            tag = data[2]
+            for use in activeUsers: #loop through all active users
+                if "#ALL" in userToHash[use] or tag in userToHash[use]: #check if tag or ALL is in their set of subscriptions
+                    userToPort[use].sendall(("rep " + str(use)+" "+ str(message)+ " " + str(tag)).encode()) #send to client to display
+
+            
+        # conn.sendall(data.enconde())
+
 def validPort(args): 
     try: 
         port = int(args[1])
